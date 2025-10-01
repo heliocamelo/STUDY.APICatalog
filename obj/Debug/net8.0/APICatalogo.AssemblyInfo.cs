ab2367088b4e5333fa25aaf2729a61f2c7138635
heliocamelo <heliotdk@gmail.com>
2025-10-01 15:10:09 -0300

    Criando ProdutosController e o metodo Ge()

--- obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5f327d96b657bc4256075451a1bad07d951bcc2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee893779db0541e605612318dc5db8923eb55f59")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
