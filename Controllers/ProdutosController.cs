7aba4569401ae80a9ea65527240b7cf997f10423
heliocamelo <heliotdk@gmail.com>
2025-10-01 15:23:39 -0300

    Criando o metodo Put()

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -1,6 +1,7 @@
 using APICatalogo.Context;
 using APICatalogo.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace APICatalogo.Controllers;
 
@@ -51,4 +52,19 @@ public class ProdutosController : Controller
         return new CreatedAtRouteResult("ObterProduto",
             new { id = produto.ProdutoId }, produto);
     }
+
+    [HttpPut("{id:int}")]
+    public ActionResult Put(int id, Produto produto)
+    {
+        if (id != produto.ProdutoId)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(produto).State = EntityState.Modified;
+        _context.SaveChanges();
+
+        return Ok(produto);
+    }
+    
 }
