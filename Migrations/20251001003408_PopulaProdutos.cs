ee893779db0541e605612318dc5db8923eb55f59
heliocamelo <heliotdk@gmail.com>
2025-09-30 21:45:28 -0300

    Populando as tabelas

---
+++ Migrations/20251001003408_PopulaProdutos.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "Insert into Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId)" +
+                "values('Coca-Cola Diet','Refrigerante de cola 350 ml',5.45,'cocacola.jpg',50,now(), 1)");
+            migrationBuilder.Sql(
+                "Insert into Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId)" +
+                "values('Lanche de Atum','Lanchde de Atum com maionese',8.50,'atum.jpg',10,now(), 2)");
+            migrationBuilder.Sql(
+                "Insert into Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId)" +
+                "values('Pudim 10g','Pudim de leite condensado 100g',6.75,'pudim.jpg',20,now(), 3)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from Produtos");
+        }
+    }
+}
--- obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+864d83508a6d76a74c664f3a2d6b600ecbc2d9ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5f327d96b657bc4256075451a1bad07d951bcc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
